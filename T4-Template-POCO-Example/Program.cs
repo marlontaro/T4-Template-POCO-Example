5ae7664db5be821b32017569e16f6b04cdae6805
Marlon Cordova <marloncordova@outlook.com>
2018-08-19 20:09:07 -0500

    generate poco ok

--- T4-Template-POCO-Example/Program.cs
+++ T4-Template-POCO-Example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace T4_Template_POCO_Example
     class Program
     {
         static void Main(string[] args)
-        {
+        {          
         }
     }
 }
